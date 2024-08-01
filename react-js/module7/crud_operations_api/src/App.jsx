a6a598fa1ffcfe47ffbadd97a1dd87cb6f6943fd
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-08-01 13:46:43 +0530

    01-08-2024 all data updated

--- react-js/module7/crud_operations_api/src/App.jsx
+++ react-js/module7/crud_operations_api/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./components/Home";
 import Create from "./components/Create";
-import Edit from "./components/Edit";
+import DeleteData from "./components/DeleteData";
+import EditData from "./components/EditData";
 import NotFound from "./components/NotFound";
 const App=()=>{
   return (
@@ -11,7 +12,8 @@ const App=()=>{
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-data" element={<Create />} />
-        <Route path="/edit-data" element={<Edit />} />
+        <Route path="/delete-data/:id" element={<DeleteData />} />
+        <Route path="/edit-data/:id" element={<EditData />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>

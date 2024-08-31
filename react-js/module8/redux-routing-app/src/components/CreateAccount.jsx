61a214d4dd8e88002f4294af0b583a0edcfed223
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-08-31 16:55:05 +0530

    31-08-2024 all data uploaded

--- react-js/module8/redux-routing-app/src/components/CreateAccount.jsx
+++ react-js/module8/redux-routing-app/src/components/CreateAccount.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import { Link } from 'react-router-dom'
 import {Container,Row} from 'react-bootstrap'
 import Header from './Header'
 import Nav from './Navbar'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom'
+
 export default function CreateAccount() {
+//destructuring of data    //
+const[data,setData]=useState([]);
+const email=useRef("");
+const password=useRef("");
+const username=useRef("");
+const phone=useRef("");
+const address=useRef("");
+const Navigate=useNavigate();
+
+// create a form Handeler 
+const addFormData=(e)=>{
+    e.preventDefault();
+    var insert={
+        email:email.current.value,
+        password:password.current.value,
+        username:username.current.value,
+        phone:phone.current.value,
+        address:address.current.value
+    }
+    // create an account and stored users
+    axios.post(`http://localhost:8000/users-auth`,insert).then(()=>{
+        Swal.fire({
+            title: "Thanks!",
+            text: "Your account created successfully!",
+            icon: "success"
+          });
+    });
+    Navigate('/login');
+
+}
 return (
 <div>
 <div>
@@ -15,28 +49,28 @@ return (
 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbV6N8JDxfOKuoh3eWCmyaR32hKnldSBN7tQ&s' alt='login' className='img-fluid' />
 </div>
 <div className='col-md-8'><h2>Create Account here</h2>
-<form>
+<form onSubmit={addFormData}>
 <div className='form-group mt-3'>
-<input type='text' placeholder='email' className='form-control' />
+<input type='text' placeholder='email' ref={email} className='form-control' />
 </div>
 
 <div className='form-group mt-3'>
-<input type='password' placeholder='password' className='form-control' />
-</div>
-
-
-<div className='form-group mt-3'>
-<input type='text' placeholder='username' className='form-control' />
+<input type='password' placeholder='password' ref={password} className='form-control' />
 </div>
 
 
 <div className='form-group mt-3'>
-<input type='text' placeholder='phone' className='form-control' />
+<input type='text' placeholder='username' ref={username} className='form-control' />
 </div>
 
 
 <div className='form-group mt-3'>
-<textarea placeholder='address' className='form-control'></textarea>
+<input type='text' placeholder='phone' ref={phone} className='form-control' />
+</div>
+
+
+<div className='form-group mt-3'>
+<textarea placeholder='address' ref={address} className='form-control'></textarea>
 </div>
 
 
